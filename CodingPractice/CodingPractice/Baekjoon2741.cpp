c9723c407c9fd256971e495c8357f1c997b99b0e
yejin <yejin3122@naver.com>
2018-08-04 03:41:37 +0900

    baekjoon2741

---
+++ CodingPractice/CodingPractice/Baekjoon2741.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int main()
+{
+	int n;
+
+	cout << "NÀÔ·Â: ";
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		cout << i << endl;
+	}
+
+	system("pause");
+	return 0;
+}
---
+++ CodingPractice/CodingPractice/HelloWorld.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int main()
+{
+	cout << "Hello yejin world!" << endl;
+	system("pause");
+	return 0;
+}
